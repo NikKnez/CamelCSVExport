ed6f42c353f9ec32a50a016a4bbddf34467e91d7
NikKnez <nknezevic07@gmail.com>
2024-10-21 12:02:16 +0200

    Initial commit

---
+++ src/main/java/CamelCSVExport/CamelCsvExportApplication.java
@@ -0,0 +1,13 @@
+package CamelCSVExport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelCsvExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelCsvExportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CamelCSVExport/CamelCsvExportApplicationTests.java
@@ -0,0 +1,13 @@
+package CamelCSVExport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelCsvExportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
